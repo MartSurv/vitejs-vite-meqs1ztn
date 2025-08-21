b8a06199e5f0ac9abcb2218feee9b7311f69f999
Martynas Survila <martynas.survila@nordsec.com>
2025-08-21 15:34:44 +0300

    add masonry layout

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -8,12 +8,22 @@ type ImageProps = {
 };
 
 export const Image: React.FC<ImageProps> = ({ image, openModal }) => {
+  // Calculate height based on aspect ratio with fixed width of 400px
+  const fixedWidth = 400;
+  const aspectRatio = image.width / image.height;
+  const calculatedHeight = Math.round(fixedWidth / aspectRatio);
+
   return (
     <div className="image-card" onClick={() => openModal(image)}>
       <img
-        src={`https://picsum.photos/id/${image.id}/400/300`}
+        src={`https://picsum.photos/id/${image.id}/${fixedWidth}/${calculatedHeight}`}
         alt={`Photo by ${image.author}`}
         loading="lazy"
+        style={{
+          width: "100%",
+          height: "auto",
+          aspectRatio: `${image.width} / ${image.height}`,
+        }}
       />
       <div className="image-info">
         <span className="author">📸 {image.author}</span>
